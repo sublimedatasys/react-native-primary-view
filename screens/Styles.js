4b7b01c9d8e09f97a35e164d6f82197571326741
Your Name <mahendra.pratap4022@gmail.com>
2019-06-02 20:57:00 +0530

    bottom margin sceen

--- screens/Styles.js
+++ screens/Styles.js
@@ -37,7 +37,7 @@ export default StyleSheet.create({
     listView:{
       marginLeft:10,
       marginRight:10,
-      marginBottom:10,
+      marginBottom:40,
       padding:10,
       borderRadius:5,
       backgroundColor:'#fafafb'
